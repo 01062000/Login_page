f55c7d54948605b6f331874795c3d2591b1187b5
Rohit Das <dasrohit162000@gmail.com>
2020-07-24 18:44:55 +0530

    Updates

---
+++ precache-manifest.d520437057b1435fb5f275ca3f013ad3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64fcf878122397821f6243f7b670e5a2",
+    "url": "/Login_page/index.html"
+  },
+  {
+    "revision": "1e5ad69b6b95cf2d5a8e",
+    "url": "/Login_page/static/css/2.d5fa9f53.chunk.css"
+  },
+  {
+    "revision": "7205ae04c6194f3b0704",
+    "url": "/Login_page/static/css/main.6257648e.chunk.css"
+  },
+  {
+    "revision": "1e5ad69b6b95cf2d5a8e",
+    "url": "/Login_page/static/js/2.beccf42e.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/Login_page/static/js/2.beccf42e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7205ae04c6194f3b0704",
+    "url": "/Login_page/static/js/main.89790cb3.chunk.js"
+  },
+  {
+    "revision": "074402ccd356e4c60675",
+    "url": "/Login_page/static/js/runtime-main.0a3a570f.js"
+  }
+]);
